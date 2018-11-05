b77d69909e5d5eadf6274cdb68d9a84ad37e13c1
WINMAC17\2daw <sergio_311096@hotmail.es>
2018-11-05 13:46:10 +0100

    subir Actividad02

---
+++ Actividad01/Ejercicio.js
@@ -0,0 +1,17 @@
+//console.log(document);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.title);
+//console.log(document.head.parentMode);
+//console.log(document.head.nextSimblig.nextSibling);
+//console.log(document.body.firstChild);
+//console.log(document.body.childNodes[1]);
+//let children = Arrays.from(document.body.childNodes);
+//console.log(typeof(children));
+//children.forEach( x => console.log(x) );
+//children.(map (x => x + x );
+//console.log( children.filter( x => x.nodeType ===1 ) );
+/*for(let child of children){
+    console.log(child);
+}*/
+console.log(document.body.div.firstChild.nextSimbling);
